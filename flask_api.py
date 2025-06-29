6ea29938330640891029f431b7344c50b2c10f6f
Buholeks <154289845+Buholeks@users.noreply.github.com>
2025-06-28 22:26:05 -0600

    mejorado

--- flask_api.py
+++ flask_api.py
@@ -4,23 +4,25 @@ from scraper_original import scrape_single_product
 import traceback
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
+
 
 @app.route("/scraper_original", methods=["POST"])
 def scrape_product():
     try:
         data = request.get_json()
         url = data.get("url")
-        
+
         if not url:
             return jsonify({"status": "error", "message": "URL requerida"}), 400
-        
+
         result = scrape_single_product(url)
         return jsonify(result)
-        
+
     except Exception as e:
         traceback.print_exc()
         return jsonify({"status": "error", "message": str(e)}), 500
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
